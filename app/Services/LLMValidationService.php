1cf023a5f320d1ddeb8227d3fb403f6b700442f4
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-06-01 22:29:15 +0800

    Changed to gemini 2.5 due to high traffic

--- app/Services/LLMValidationService.php
+++ app/Services/LLMValidationService.php
@@ -27,7 +27,7 @@ class LLMValidationService
 
            // Using 3.5 Flash
             $response = Http::withHeaders(['Content-Type' => 'application/json'])
-                ->post("https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key={$apiKey}", [
+                ->post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={$apiKey}", [
                     'contents' => [
                         'parts' => [
                             ['text' => $prompt]
